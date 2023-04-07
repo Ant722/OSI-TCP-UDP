bfcd43c44201950bdc9c47e8fa8d414f022ddb54
Ant722 <Ant722@mail.ru>
2023-04-07 11:34:35 +0300

    creat class

--- src/main/java/org/example/Client3.java
+++ src/main/java/org/example/Client3.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import static org.example.ServerConfig.HOST;
+import static org.example.ServerConfig.PORT;
+
 public class Client3 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        try(Socket socket = new Socket(HOST,PORT);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(),true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+            System.out.println(reader.readLine());
+            writer.println("Helloow from client");
+        }
+
     }
 }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        try(ServerSocket server = new ServerSocket(ServerConfig.PORT)) {
+
+            System.out.println("Сервер запущен");
+
+            while (true){
+                try(Socket client = server.accept();
+                    PrintWriter writer = new PrintWriter(client.getOutputStream(),true);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()))){
+
+                    System.out.println("Подключен клиент" + client.getPort());
+
+                    writer.println("Hi from server!");
+                    System.out.println(reader.readLine());
+                }
+            }
+        }
     }
 }
